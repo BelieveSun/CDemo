8a031cdd80d77da9638362463f362133f25358fd
BelieveSun <trustsun@163.com>
2018-07-10 14:01:38 +0800

    add demo7

--- src/demo4.cpp
+++ src/demo4.cpp
@@ -11,25 +11,25 @@ void demo5();
 
 void demo6();
 
+void demo7();
+
 /**
  * b的内存地址不一样
  * @return
  */
 int main() {
-    int x = 56;
-    int &a = x;
-    int &r = a;
-    int b = x;
-    cout << "x =" << x << " , &x=" << &x << ",a=" << a << ", &a=" << &a << ",r=" << r << " ,&r=" << &r
-         << " ,b=" << b << ",&b=" << &b << endl;
-    r = 25;
-    cout << "x =" << x << " , &x=" << &x << ",a=" << a << ", &a=" << &a << ",r=" << r << " ,&r=" << &r
-         << " ,b=" << b << ",&b=" << &b << endl;
-    demo5();
-    demo6();
+//    int x = 56;
+//    int &a = x;
+//    int &r = a;
+//    int b = x;
+//    cout << "x =" << x << " , &x=" << &x << ",a=" << a << ", &a=" << &a << ",r=" << r << " ,&r=" << &r
+//         << " ,b=" << b << ",&b=" << &b << endl;
+//    r = 25;
+//    cout << "x =" << x << " , &x=" << &x << ",a=" << a << ", &a=" << &a << ",r=" << r << " ,&r=" << &r
+//         << " ,b=" << b << ",&b=" << &b << endl;
+    demo7();
     return 0;
 }
-
 
 
 void demo5() {
@@ -57,4 +57,22 @@ void demo6() {
     reverse_copy(a, a + 4, b);
     copy(b, b + 4, ostream_iterator<double>(cout, " "));
     cout << endl;
+}
+
+void demo7() {
+    double a[] = {1.1, 4.4, 3.3, 2.2};
+    sort(a, a + 4);
+    copy(a, a + 4, ostream_iterator<double>(cout, " "));
+    cout << endl;
+    sort(a, a + 4, greater<double>());
+    copy(a, a + 4, ostream_iterator<double>(cout, " "));
+    cout << endl;
+    double * x = find(a, a + 4, 4.4);
+    if(x == a+4) cout << "没有值为4.4的数组元素";
+    else         cout << "有值为" << * x << "的数组元素";
+    cout << endl;
+    x = find(a,a+4,8);
+    if(x == a + 4) cout << "没有值为8的数组元素";
+    else           cout << "有值为" << * x << "的数组元素";
+
 }
